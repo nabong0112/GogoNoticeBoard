497998435b4ff46ebe88faa09a21f2ad58a8df37
nabong0112 <nabong0112@naver.com>
2019-07-23 20:48:03 +0900

    2019-07-23 정리

---
+++ src/main/web/WriteServlet.java
@@ -0,0 +1,79 @@
+package main.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import main.dao.WriteDao;
+import main.vo.WriteVo;
+import sun.nio.cs.HistoricallyNamedCharset;
+
+/**
+ * Servlet implementation class WriteServlet
+ */
+@WebServlet("/WriteServlet")
+public class WriteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WriteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//글쓰기에서 가지고온 문자들
+		request.setCharacterEncoding("utf-8");	
+		WriteVo vo = new WriteVo();
+		
+		String title = request.getParameter("title");
+		String text = request.getParameter("text");
+		
+		int check = 1;
+		//양쪽 공백 제거
+		String title2 = title.trim();
+		String text2 = text.trim();
+
+		System.out.println(title2.length());
+		System.out.println(text2.length());
+		System.out.println(title2);
+		System.out.println(text2);
+		
+		//글쓰기 공백 검사
+		if(title2.length() == 0 || text2.length() == 0) {
+			System.out.println(title2 + "에 공백만 들어있음");
+			System.out.println(text2 + "에 공백만 들어있음");
+			response.sendRedirect("/Nabong_writer/writeform.jsp");
+		} else {
+			
+			vo.setTitle(title2);
+			vo.setText(text2);
+			
+			WriteDao dao = new WriteDao();
+			dao.insertText(vo);
+			response.sendRedirect("/Nabong_writer/noticeboard.jsp");
+		}
+		
+		
+		
+		
+	}
+
+}
